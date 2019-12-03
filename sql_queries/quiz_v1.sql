1ffaa04a4d1c6093d0aa4dbfcf65384bffe61168
krishna192168 <krishnamoorthy.bavanudoss@revature.com>
2019-12-03 18:49:40 +0530

    naming convention updated

--- sql_queries/quiz_v1.sql
+++ sql_queries/quiz_v1.sql
@@ -1,6 +1,6 @@
 CREATE SCHEMA revature_quiz_db;
 USE revature_quiz_db;
-CREATE TABLE quiz(
+CREATE TABLE quizs(
 	id int AUTO_INCREMENT,
     name varchar(50) NOT NULL,
     tags varchar(300) NOT NULL,
@@ -27,36 +27,40 @@ CREATE TABLE quiz(
     show_correct_answer bool,
     show_answer_explanation bool DEFAULT 0,
     is_save_and_resume bool DEFAULT 1,
+    create_on timestamp DEFAULT CURRENT_TIMESTAMP,
+    updated_on timestamp,
+    created_by varchar(50) NOT NULL,
+    modified_by varchar(50),
     CONSTRAINT pk_quiz PRIMARY KEY(id),
     CONSTRAINT ck_activity_points CHECK(activity_point >= 0),
     CONSTRAINT ck_max_number_of_attempts CHECK(max_number_of_attempts > 0),
     CONSTRAINT fk_level_id FOREIGN KEY(level_id) REFERENCES level(id),
     CONSTRAINT fk_category_id FOREIGN KEY(category_id) REFERENCES category(id)
 );
-CREATE TABLE quiz_pool(
+CREATE TABLE quiz_pools(
 	id int AUTO_INCREMENT,
     name varchar(25) NOT NULL,
     max_number_of_questions int,
     quiz_id int NOT NULL,
     CONSTRAINT pk_quiz_pool PRIMARY KEY(id),
-    CONSTRAINT fk_quiz_id FOREIGN KEY(quiz_id) REFERENCES quiz(id)
+    CONSTRAINT fk_quiz_id FOREIGN KEY(quiz_id) REFERENCES quizs(id)
 );
-CREATE TABLE quiz_pool_question(
+CREATE TABLE quiz_pool_questions(
 	id int AUTO_INCREMENT,
     question_id int NOT NULL,
     quiz_pool_id int NOT NULL,
     is_sticky bool,
     is_evaluate bool,
     CONSTRAINT pk_quiz_pool_question PRIMARY KEY(id),
-    CONSTRAINT fk_quiz_pool_id FOREIGN KEY(quiz_pool_id) REFERENCES quiz_pool(id)
+    CONSTRAINT fk_quiz_pool_id FOREIGN KEY(quiz_pool_id) REFERENCES quiz_pools(id)
 );
-CREATE TABLE level(
+CREATE TABLE levels(
 	id int AUTO_INCREMENT,
     level_name varchar(50) NOT NULL,
     description varchar(500),
     CONSTRAINT pk_level_id PRIMARY KEY(id)
 );
-CREATE TABLE category(
+CREATE TABLE categories(
 	id int AUTO_INCREMENT,
     category_name varchar(50) NOT NULL,
     icon_url varchar(500),
