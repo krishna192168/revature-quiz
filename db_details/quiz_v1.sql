a4bc3bf69aa74bf91cd07b8e74117449e9805ae0
krishna192168 <krishnamoorthy.bavanudoss@revature.com>
2019-12-10 11:54:01 +0530

    updated database detatils

--- db_details/quiz_v1.sql
+++ db_details/quiz_v1.sql
@@ -1,5 +1,20 @@
-CREATE SCHEMA revature_quiz_db;
-USE revature_quiz_db;
+CREATE SCHEMA quiz;
+USE quiz;
+CREATE TABLE levels(
+	id int AUTO_INCREMENT,
+    level_name varchar(50) NOT NULL,
+    description varchar(500),
+    CONSTRAINT pk_level_id PRIMARY KEY(id)
+);
+ALTER TABLE levels CHANGE level_name name varchar(50); 
+CREATE TABLE categories(
+	id int AUTO_INCREMENT,
+    category_name varchar(50) NOT NULL,
+    icon_url varchar(500),
+    description varchar(500),
+    CONSTRAINT pk_category_id PRIMARY KEY(id)
+);
+ALTER TABLE categories CHANGE category_name name varchar(50); 
 CREATE TABLE quizzes(
 	id int AUTO_INCREMENT,
     name varchar(50) NOT NULL,
@@ -29,14 +44,19 @@ CREATE TABLE quizzes(
     is_save_and_resume bool DEFAULT 1,
     create_on timestamp DEFAULT CURRENT_TIMESTAMP,
     updated_on timestamp,
-    created_by varchar(50) NOT NULL,
+    created_by int NOT NULL,
     modified_by varchar(50),
     CONSTRAINT pk_quiz PRIMARY KEY(id),
     CONSTRAINT ck_activity_points CHECK(activity_point >= 0),
     CONSTRAINT ck_max_number_of_attempts CHECK(max_number_of_attempts > 0),
-    CONSTRAINT fk_level_id FOREIGN KEY(level_id) REFERENCES level(id),
-    CONSTRAINT fk_category_id FOREIGN KEY(category_id) REFERENCES category(id)
+    CONSTRAINT fk_level_id FOREIGN KEY(level_id) REFERENCES levels(id),
+    CONSTRAINT fk_category_id FOREIGN KEY(category_id) REFERENCES categories(id),
+    CONSTRAINT fk_created_by FOREIGN KEY(created_by) REFERENCES employees(id),
+    CONSTRAINT uk_quiz_name UNIQUE(name)
 );
+ALTER TABLE quizzes ADD version int NOT NULL DEFAULT 1;
+ALTER TABLE quizzes CHANGE create_on created_on varchar(50);
+ALTER TABLE quizzes ADD is_status bool DEFAULT 0;
 CREATE TABLE quiz_pools(
 	id int AUTO_INCREMENT,
     name varchar(25) NOT NULL,
@@ -54,16 +74,19 @@ CREATE TABLE quiz_pool_questions(
     CONSTRAINT pk_quiz_pool_question PRIMARY KEY(id),
     CONSTRAINT fk_quiz_pool_id FOREIGN KEY(quiz_pool_id) REFERENCES quiz_pools(id)
 );
-CREATE TABLE levels(
+CREATE TABLE employees(
 	id int AUTO_INCREMENT,
-    level_name varchar(50) NOT NULL,
-    description varchar(500),
-    CONSTRAINT pk_level_id PRIMARY KEY(id)
+    name varchar(50),
+    email varchar(50),
+    password varchar(20),
+    role varchar(20),
+    createdOn timestamp DEFAULT CURRENT_TIMESTAMP,
+    modifiedOn timestamp,
+    createdBy varchar(50),
+    modifiedBy varchar(50),
+    PRIMARY KEY(id)
 );
-CREATE TABLE categories(
-	id int AUTO_INCREMENT,
-    category_name varchar(50) NOT NULL,
-    icon_url varchar(500),
-    description varchar(500),
-    CONSTRAINT pk_category_id PRIMARY KEY(id)
-);
+ALTER TABLE employees ADD isActive bool;
+DROP TABLE quizzes;
+
+select id from quizzes where id = 1 or 1=1;
